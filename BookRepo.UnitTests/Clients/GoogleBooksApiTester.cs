df8af3e73013b4a2503b9736b0d71856cbdaabd9
mercurysn <sdhf452erh>
2015-07-21 23:06:30 +1000

    Adding in RestSharp to query Google Books API

---
+++ BookRepo.UnitTests/Clients/GoogleBooksApiTester.cs
@@ -0,0 +1,28 @@
+﻿using BookRepo.Clients;
+using NUnit.Framework;
+using RestSharp;
+
+namespace BookRepo.UnitTests.Clients
+{
+    [TestFixture]
+    public class GoogleBooksApiTester
+    {
+        [Test]
+        public void GoogleBooksApi_IntegrationTests()
+        {
+            GoogleBooksApi api =
+                new GoogleBooksApi(
+                    "https://www.googleapis.com");
+
+            //var result = api.Execute("books/v1/volumes/ItvZr-OV0DEC?key=AIzaSyDhHJkRg7Yv6Z4hpw0OGsuMUl_WIlWpj20", Method.GET);
+            var googleBook = api.GetGoogleBookApiResult("books/v1/volumes/ItvZr-OV0DEC?key=AIzaSyDhHJkRg7Yv6Z4hpw0OGsuMUl_WIlWpj20", Method.GET);
+
+            Assert.AreEqual("books#volume", googleBook.Kind);   
+            Assert.AreEqual("The Last Man", googleBook.VolumeInfo.Title);
+            Assert.AreEqual("Simon and Schuster", googleBook.VolumeInfo.Publisher);
+            Assert.AreEqual("9781439100530", googleBook.VolumeInfo.IndustryIdentifiers[1].Identifier);
+            Assert.AreEqual("9781439100530", googleBook.VolumeInfo.Isbn13);
+            Assert.AreEqual("1439100535", googleBook.VolumeInfo.Isbn10);
+        }
+    }
+}
---
+++ BookRepo/Clients/GoogleBooksApi.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+
+namespace BookRepo.Clients
+{
+    public class GoogleBooksApi
+    {
+        private readonly IRestClient _client = new RestClient();
+
+        public GoogleBooksApi(string url)
+        {
+            _client = new RestClient(url);
+        }
+
+        public GoogleBooksApi(IRestClient client)
+        {
+            _client = client;
+        }
+
+        public IRestResponse Execute(string url, Method method)
+        {
+            RestRequest request = new RestRequest(url, method);
+
+            return _client.Execute(request);
+
+            //https://www.googleapis.com/books/v1/volumes/ItvZr-OV0DEC?key=AIzaSyDhHJkRg7Yv6Z4hpw0OGsuMUl_WIlWpj20
+        }
+
+        public GoogleBook GetGoogleBookApiResult(string url, Method method)
+        {
+            RestRequest request = new RestRequest(url, method);
+
+            var response = _client.Execute<GoogleBook>(request);
+
+            response.Data = MapIsbn(response.Data);
+
+            return response.Data;
+        }
+
+        private GoogleBook MapIsbn(GoogleBook googleBook)
+        {
+            foreach (var identifier in googleBook.VolumeInfo.IndustryIdentifiers)
+            {
+                switch (identifier.Type)
+                {
+                    case "ISBN_10":
+                        googleBook.VolumeInfo.Isbn10 = identifier.Identifier;
+                        break;
+                    case "ISBN_13":
+                        googleBook.VolumeInfo.Isbn13 = identifier.Identifier;
+                        break;
+
+                }
+            }
+
+            return googleBook;
+        }
+
+
+    }
+}
